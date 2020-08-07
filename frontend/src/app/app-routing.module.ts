7d1ad7a96876f98d242e2dfbcb11040723545401
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-08-06 22:22:29 -0600

    app layout and styles

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
+
+
+const routes: Routes = [
+  {path: '', component: TaskViewComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
