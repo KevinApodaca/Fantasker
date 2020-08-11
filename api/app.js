8ecc12921bb2c12b0b3c7316919f5e49e5a7519f
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-08-10 19:53:04 -0600

    login method works

--- api/app.js
+++ api/app.js
@@ -14,6 +14,11 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    res.header(
+        'Access-Control-Expose-Headers',
+        'x-access-token, x-refresh-token'
+    );
     next();
 });
 
---
+++ frontend/src/app/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { WebRequestService } from './web-request.service';
+import { Router } from '@angular/router';
+import { shareReplay, tap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private webService: WebRequestService, private router: Router) { }
+
+  /* Make request with email and password from input */
+  login(email: string, password: string) {
+    return this.webService.login(email, password).pipe(
+      shareReplay(),
+      tap((res: HttpResponse<any>) => {
+        this.setSession(res.body._id, res.headers.get('x-access-token'), res.headers.get('x-refresh-token'));
+        console.log('logged in');
+      })
+    )
+  }
+
+  logout() {
+    this.removeSession();
+  }
+
+  private setSession(userId: string, accessToken: string, refreshToken: string) {
+    localStorage.setItem('user-id', userId);
+    localStorage.setItem('access-token', accessToken);
+    localStorage.setItem('refresh-token', refreshToken);
+  }
+
+  private removeSession() {
+    localStorage.removeItem('user-id');
+    localStorage.removeItem('access-token');
+    localStorage.removeItem('refresh-token');
+  }
+}
